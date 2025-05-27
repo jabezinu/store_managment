40b32b861600694e13163353dc6818ef439e3e8c
jabezinu <jabezzinu31@gmail.com>
2025-05-28 01:21:54 +0300

    divide the api further into routes and controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,83 +1,16 @@
 import express from "express";
-import Product from "../models/product.model.js";
-import mongoose from "mongoose";
+import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from "../controllers/product.controller.js";
 
 const productRoutes = express.Router();
 
-productRoutes.get("/", async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json({success: true, data: products})
-    } catch (error) {
-        console.error(`Error on the Product viewing ${error.message}`);
-        res.status(500).json({success: false, message: "Server Error"});        
-    }
-})
+productRoutes.get("/", getAllProducts)
 
-productRoutes.post("/", async (req, res) => {
-    const product = req.body;
-    
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "please fill all filds"})
-    }
-    
-    const newProduct  = new Product(product)
-    
-    try {
-        await newProduct.save();
-        return res.status(201).json({success: true, data: newProduct})
-    } catch (error) {
-        console.error(`Error on the product creatig ${error.massage}`);        
-        res.status(500).json({success: false, message: "Server Error"})
-    }
-})
+productRoutes.post("/", createProduct)
 
-productRoutes.get("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const products = await Product.findById(id);
-        res.status(200).json({success: true, data: products})
-    } catch (error) {
-        console.error(`Error on the Product viewing ${error.message}`);
-        res.status(500).json({success: false, message: "Server Error"});        
-    }
-})
+productRoutes.get("/:id", getProduct)
 
-productRoutes.put("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const {name, price, image} = req.body;
+productRoutes.put("/:id", updateProduct)
 
-        if(!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({success: false, message: "no such product"})
-        }
-
-        const updatedProducts = await Product.findByIdAndUpdate(id,{
-            name,
-            price,
-            image,
-        },{ new: true});
-
-        res.status(200).json({success: true, data: updatedProducts})
-    } catch (error) {
-        console.error(`Error on the Product viewing ${error.message}`);
-        res.status(500).json({success: false, message: "Server Error"});        
-    }
-})
-
-productRoutes.delete("/:id", async (req, res) => {
-    try {
-        const {id} = req.params;        
-        
-        await Product.findByIdAndDelete(id);
-
-        res.status(200).json({success: true, message: "product deleted"})
-        console.log("text for testing");
-        
-    } catch (error) {
-        console.error(`Error on the product deletion ${error.massage}`);        
-        res.status(500).json({success: false, message: "Server Error"})
-    }
-})
+productRoutes.delete("/:id", deleteProduct)
 
 export default productRoutes;
