1fc83212d55063002785c8ec5fece5e90a0b1931
jabezinu <jabezzinu31@gmail.com>
2025-05-28 09:59:45 +0300

    working on the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import { Container,  SimpleGrid,  Text, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useProductStore } from '../store/product';
+import ProductCard from '../components/ProductCard';
 
 const HomePage = () => {
-  return (
-    <div>
-      Home
-    </div>
+
+  const {fetchProducts, products} = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("prodcts", products);
+  return ( 
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8} >
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip="text"
+          textAlign="center"
+        >
+          Current Product 🚀
+        </Text>
+
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        <Text fontsize='xl' textAlign={"center"} fontWeight={"bold"} color="gray.500">
+          No Products found 😢{" "}
+          <Link to={"/create"}>
+            <Text as="span" color="blue.500" _hover={{ textAlign: "underline" }}>
+              <Link to={"/create"}>Create Product.</Link>
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
