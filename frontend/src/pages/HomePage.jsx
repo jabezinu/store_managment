8fee0fe0bf9abd857fe2676bcf70f99886de12b6
jabezinu <jabezzinu31@gmail.com>
2025-05-29 10:12:29 +0300

    add the model on home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,15 +16,7 @@ const HomePage = () => {
   return ( 
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8} >
-        <Text
-          fontSize={"30"}
-          fontWeight={"bold"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip="text"
-          textAlign="center"
-        >
-          Current Product 🚀
-        </Text>
+        <Text fontSize={"30"} fontWeight={"bold"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip="text" textAlign="center" >Current Product 🚀</Text>
 
         <SimpleGrid
           columns={{ base: 1, md: 2, lg: 3 }}
